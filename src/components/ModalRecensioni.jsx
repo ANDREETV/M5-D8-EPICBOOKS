a8e9979b319d2000d9b2c37c5ce03a3a662cafd3
AndreeTv <iorioandreaivrea@gmail.com>
2023-07-20 01:38:49 +0200

    EDIT

--- src/components/ModalRecensioni.jsx
+++ src/components/ModalRecensioni.jsx
@@ -1,198 +1,108 @@
-// import axios from 'axios';
-// import React from 'react'
-// import { useState, useEffect } from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import Modal from 'react-bootstrap/Modal';
-
-// const booksAPI = 'https://striveschool-api.herokuapp.com/api/comments/'
-// const autoAPI = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNkNDRkMzZhMTllZjAwMTRkNmU0NTEiLCJpYXQiOjE2ODQzNDM5MDksImV4cCI6MTY4NTU1MzUwOX0.T1tPFlolGMY_u6vxmS37K7M7o1z8bt8LUAPxfrae2u4'
-
-
-
-// export default function ModalRecensioni({ elementId }) {
-//     const [comments, setComments] = useState([])
-//     const [review, setReview] = useState("");
-//     const [rating, setRating] = useState("");
-
-//     const [show, setShow] = useState(false);
-//     const handleClose = (e) => { setShow(false); e.stopPropagation() };
-//     const handleShow = (e) => { setShow(true); e.stopPropagation() };
-
-//     function handleReviewChange(e) {
-//         setReview(e.target.value);
-//     }
-
-//     function handleRatingChange(e) {
-//         setRating(e.target.value);
-//     }
-
-
-//     function handleSubmit(e) {
-//         e.preventDefault();
-//         const newComment = {
-//             elementId: elementId,
-//             comment: review,
-//             rate: rating,
-//         }
-
-//         const config = {
-//             headers: {
-//                 Authorization: `Bearer ${autoAPI}`
-//             }
-//         }
-//             axios.post(booksAPI, newComment, config)
-//                 .then((response) => {
-//                     setComments([...comments, response.data])
-//                 })
-//                 .catch((error) => console.log("errore post", error));
-
-//     }
-
-//     return (
-//         <>
-//             <Button variant="primary" onClick={handleShow}>
-//                 Launch demo modal
-//             </Button>
-
-//             <Modal show={show} onHide={handleClose}>
-//                 <Modal.Header closeButton>
-//                     <Modal.Title>Inserisci recensione</Modal.Title>
-//                 </Modal.Header>
-//                 <Modal.Body>
-//                     <Form onSubmit={handleSubmit} >
-//                         <Form.Group className="mb-3">
-//                             <Form.Label>Scrivi recensione</Form.Label>
-//                             <Form.Control type="text" placeholder="Scrivi recensione" name="review" value={review} onChange={handleReviewChange} onClick={(e) => e.stopPropagation()} />
-//                         </Form.Group>
-//                         <Form.Group className="mb-3">
-//                             <Form.Label>Valutazione (da 1 a 5)</Form.Label>
-//                             <Form.Control type="number" placeholder="" name="rating" value={rating} onChange={handleRatingChange} onClick={(e) => e.stopPropagation()} />
-//                         </Form.Group>
-//                         {/* {error && <p style={{ color: "red" }}>{error}</p>} */}
-//                         <Button variant="primary" type="submit" onClick={handleSubmit}>
-//                             Invia recensione
-//                         </Button>
-//                     </Form>
-//                 </Modal.Body>
-//                 <Modal.Footer>
-//                     <Button variant="secondary" onClick={handleClose}>
-//                         Close
-//                     </Button>
-//                 </Modal.Footer>
-//             </Modal>
-//         </>
-//     )
-// }
 import axios from 'axios';
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
-const booksAPI = 'https://striveschool-api.herokuapp.com/api/comments/'
-const autoAPI = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM2ZjI1ODMzYjE1MjAwMTQ3NjE3OWIiLCJpYXQiOjE2ODcyNjU0NjMsImV4cCI6MTY4ODQ3NTA2M30.CGk44f84KTI-ByU4Ma5Bb14qgj74Dv887zIzabyL_XQ"
+const booksAPI = 'https://striveschool-api.herokuapp.com/api/comments/';
+const autoAPI =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM2ZjI1ODMzYjE1MjAwMTQ3NjE3OWIiLCJpYXQiOjE2ODcyNjU0NjMsImV4cCI6MTY4ODQ3NTA2M30.CGk44f84KTI-ByU4Ma5Bb14qgj74Dv887zIzabyL_XQ';
 
+export default function ModalRecensioni({ comments, setComments, elementId }) {
+  const [review, setReview] = useState('');
+  const [rating, setRating] = useState('');
+  const [erroriRec, setError] = useState(null);
+  const [selectedComment, setSelectedComment] = useState(null);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
 
+  function handleShow() {
+    setShow(true);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-export default function ModalRecensioni({ comments,setComments,elementId }) {
-    const [review, setReview] = useState("");
-    const [rating, setRating] = useState("");
-    const [erroriRec, setError] = useState(null);
-    const [selectedComment, setSelectedComment] = useState(null);
-    const [show, setShow] = useState(false);
-    const handleClose = () =>  setShow(false);  
-    // const handleShow = () => setShow(true);  
-    
-    function handleShow() {
-        setShow(true)
+    if ((rating < 1 || rating > 5 || rating === '') && review === '') {
+      return setError('Devi scrivere la recensione e dare un voto');
+    } else if (rating < 1 || rating > 5) {
+      return setError('La valutazione deve essere compresa tra 1 e 5');
+    } else if (review === '') {
+      return setError('Devi scrivere la recensione');
     }
-    const handleSubmit = (e) => {
-        e.preventDefault();
 
-        if ((rating < 1 || rating > 5 || rating === '') && (review === '')) {
-            return setError('Devi scrivere la recensione e dare un voto');
-        } else if (rating < 1 || rating > 5) {
-            return setError("La valutazione deve essere compresa tra 1 e 5");
-        } else if (review === '') {
-            return setError("Devi scrivere la recensione");
-        }
-
-        const newComment = {
-            elementId: elementId,
-            comment: review,
-            rate: rating,
-        };
-
-        console.log("ddadad",selectedComment);
-
-            // Updating existing comment
-            axios({
-                method: 'post',
-                url: booksAPI,
-                data: newComment,
-                headers: {
-                    Authorization: `Bearer ${autoAPI}`,
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((res) => {
-                    setComments([...comments, res.data]);
-                    setReview("");
-                    setRating("");
-                })
-                .catch((error) => console.log(error));
-        
-            // Creating new comment
-
-        handleClose()
+    const newComment = {
+      elementId: elementId,
+      comment: review,
+      rate: rating,
     };
 
-    return (
-        <>
-            <Button className='w-75' variant="primary" onClick={handleShow}>
-                Aggiungi recensione
-            </Button>
+    console.log('ddadad', selectedComment);
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Inserisci recensione</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form onSubmit={handleSubmit} >
-                        <Form.Group className="mb-3 d-flex flex-column">
-                            <Form.Label>Scrivi recensione</Form.Label>
-                            <input
-                                className='my-2'
-                                type="text"
-                                name="review"
-                                value={review}
-                                onChange={(e) => setReview(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3 d-flex flex-column">
-                            <Form.Label>Valutazione (da 1 a 5)</Form.Label>
-                            <input
-                                className='my-2'
-                                type="text"
-                                name="rating"
-                                value={rating}
-                                onChange={(e) => setRating(e.target.value)}
-                            />
-                        </Form.Group>
-                        {erroriRec && <p style={{ color: "red" }}>{erroriRec}</p>}
-                        <Button variant="primary" type="submit" onClick={handleSubmit}>
-                            Invia recensione
-                        </Button>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    )
+    axios({
+      method: 'post',
+      url: booksAPI,
+      data: newComment,
+      headers: {
+        Authorization: `Bearer ${autoAPI}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setComments([...comments, res.data]);
+        setReview('');
+        setRating('');
+      })
+      .catch((error) => console.log(error));
+
+    handleClose();
+  };
+
+  return (
+    <>
+      <Button className="w-75" variant="primary" onClick={handleShow}>
+        Aggiungi recensione
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Inserisci recensione</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3 d-flex flex-column">
+              <Form.Label>Scrivi recensione</Form.Label>
+              <input
+                className="my-2"
+                type="text"
+                name="review"
+                value={review}
+                onChange={(e) => setReview(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 d-flex flex-column">
+              <Form.Label>Valutazione (da 1 a 5)</Form.Label>
+              <input
+                className="my-2"
+                type="text"
+                name="rating"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+              />
+            </Form.Group>
+            {erroriRec && <p style={{ color: 'red' }}>{erroriRec}</p>}
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Invia recensione
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,32 +1,29 @@
-
-import React from 'react'
+import React from 'react';
 
 //  bootstrap
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Button from 'react-bootstrap/Button';
 
-
-export default function Welcome({films, selectFilms}) {
-    // console.log("guarda", films);
-    return (
-        <Container>
-            <Row className='mt-5'>
-                <Col>
-                    <h1>EpiBooks</h1>
-                    <select className='mt-3' onChange={selectFilms} name="films">
-                        <option value={0}>Seleziona la categoria...</option>
-                        {films.map(ele => (
-                            <option key={ele.id} value={ele.id}>{ele.name}</option>
-                        ))}
-                    </select>
-                </Col >
-                <Col className='d-flex flex-column justify-content-end align-items-center'>
-                    <h2>Vendiamo libri di tutte le categorie</h2>
-                </Col>
-            </Row>
-        </Container>
-    )
+export default function Welcome({ films, selectFilms }) {
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col>
+          <h1>EpiBooks</h1>
+          <select className="mt-3" onChange={selectFilms} name="films">
+            <option value={0}>Seleziona la categoria...</option>
+            {films.map((ele) => (
+              <option key={ele.id} value={ele.id}>
+                {ele.name}
+              </option>
+            ))}
+          </select>
+        </Col>
+        <Col className="d-flex flex-column justify-content-end align-items-center">
+          <h2>Vendiamo libri di tutte le categorie</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
